3f79319f471b30529bfb2556e5e3dceea763310f
Saheli Mondal <sahelimondal7845@gmail.com>
2026-04-25 15:16:58 +0530

    fix:mobile responsiveness

--- components/home/HomeHero.tsx
+++ components/home/HomeHero.tsx
@@ -160,27 +160,7 @@ export default function HomeHero() {
                 View Resume
               </a>
 
-              <a
-                href="/assets/Saheli_Resume.pdf"
-                download="Saheli_Mondal_Resume.pdf"
-                className="px-8 py-3 cursor-pointer rounded-full border border-slate-500 text-slate-400 sm:text-md hover:bg-slate-900 hover:text-white transition flex items-center gap-2"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-4 h-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
-                  />
-                </svg>
-                Download Resume
-              </a>
+              
             </div>
           )}
 
